43e7952237bc06b7c07f250fc806f68cfb9e5c3a
A-Masava <140965640+A-Masava@users.noreply.github.com>
2026-05-26 13:05:33 +0600

    Update problem.py

--- Task 1/problem.py
+++ Task 1/problem.py
@@ -1,1 +1,13 @@
+nums = input().split()
 
+if len(nums) == 0:
+    print(-1)
+else:
+    nums = list(map(int, nums))
+    nums = list(set(nums))
+
+    if len(nums) < 2:
+        print(-1)
+    else:
+        nums.sort()
+        print(nums[-2])
