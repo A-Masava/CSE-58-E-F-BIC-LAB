f89fe6766ca13cdd26049519aac297458e12bbd7
A-Masava <140965640+A-Masava@users.noreply.github.com>
2026-06-09 16:09:43 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
 
+dna=input()
+
+result= ""
+
+for ch in dna:
+    if ch=='A':
+        result+='T'
+    elif ch=='T':
+        result +='A'
+    elif ch=='C':
+        result +='G'
+    else:
+        result +='C'
+print(result[::-1])
