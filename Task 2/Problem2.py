cc164490e1d5cb492cab47298c8f463c25ce8688
A-Masava <140965640+A-Masava@users.noreply.github.com>
2026-06-09 16:09:12 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,13 @@
 
+t=input().strip()
+k=int(input())
+count={}
+
+for i in range(len(t)-k+1):
+    pattern=t[i:i+k]
+    count[pattern]=count.get(pattern,0)+1
+max_count=max(count.values())
+    
+for pattern in count:
+        if count[pattern]==max_count:
+            print(pattern, end=" ")
