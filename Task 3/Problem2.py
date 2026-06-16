d2f1de9f7f641513bfba2274ba8ccd0127fc3069
A-Masava <140965640+A-Masava@users.noreply.github.com>
2026-06-16 16:18:30 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,16 @@
 
+pattern= input().strip()
+text=input().strip()
+d=int(input())
+k=len(pattern)
+ans=[]
+
+for i in range(len(text)-k+1):
+    mismatch=0
+    for j in range(k):
+        if text[i+j]!=pattern[j]:
+            mismatch=mismatch+1
+    if mismatch<=d:
+        ans.append(i)
+        
+print(*ans)
